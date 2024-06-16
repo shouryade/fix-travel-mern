63a89eae2745e04aad6d077f359cf320690d2ac4
Pranav <pranavangrishmusic@gmail.com>
2024-06-16 20:46:40 +0530

    create Pages and Push

---
+++ BlogPorject/src/routing/Routing.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import AboutUs from '../pages/AboutUs'
+import Dashboard from '../pages/Dashboard'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
+import Projects from '../pages/Projects'
+
+
+function Routing() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<AboutUs />} />
+
+        
+    </Routes>
+    
+   
+  )
+}
+
+export default Routing
