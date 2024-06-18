56df6f4db4aac23df0b881eef2ba464d559923d1
Pranav <pranavangrishmusic@gmail.com>
2024-06-18 15:16:38 +0530

    mongoose,mongoSetupWithLink,.envSetup

--- API/index.js
+++ API/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express()
 const port = 3000
+
+dotenv.config();
+
+
+mongoose.connect(
+    process.env.MONGODB
+).then(()=>{
+    console.log('Connected to the database');
+}
+    
+).catch((e)=>{
+    console.log('Error connecting to the database');
+    console.log(e);
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
