c36165ab2b524042239b3b8a6db01b345b3d8f4f
Pranav <pranavangrishmusic@gmail.com>
2024-06-18 16:22:33 +0530

    auth and hash password

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const User = require('../models/user_model');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-module.exports.signup = (req , res) => {
-    console.log(req.body);
+module.exports.signup = async (req , res) => {
+    const { userName, email, password } = req.body;
+
+    
+    
+
+    if (!userName || !email || !password || userName === '' || email === '' || password === '') {
+        return res.status(400).json({ message: 'Please fill all fields' });
+
+    }
+
+    var salt = bcrypt.genSaltSync(10);
+    var hashedPassword = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+        userName: userName,
+        email: email,
+        password: hashedPassword
+    })
+    console.log(newUser)
+
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json('User saved');
+    }catch{
+        res.status(400).json({message: 'User not saved'});
+    }
 }
