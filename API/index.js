cec71d16aee471838691945185005a8fb73a9765
Samkit Jaina <70890479+gatopotato@users.noreply.github.com>
2024-07-04 23:35:10 +0530

    Update index.js changed origin

--- API/index.js
+++ API/index.js
@@ -14,7 +14,8 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'https://midorchard-client.vercel.app', // Adjust this to your frontend URL
+  // origin: 'https://midorchard-client.vercel.app', // Adjust this to your frontend URL
+  origin: *
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
