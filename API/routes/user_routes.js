28bff3f93962ac7eba8ef322ebb5deffcf3107ab
Pranav <pranavangrishmusic@gmail.com>
2024-07-04 21:53:07 +0530

    more

--- API/routes/user_routes.js
+++ API/routes/user_routes.js
@@ -3,7 +3,9 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-
+router.get('/',(req,res)=>{
+    res.send("Hello World");
+});
 router.get('/users/:id/verify/:token',userController.verifyEmail);
 
 
